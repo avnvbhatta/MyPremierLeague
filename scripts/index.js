066f874a65474771ddc2666e0068705133b9533d
abhatta <avnvbhatta@gmail.com>
2020-11-07 10:31:50 -0500

    init

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+//Login Form Submit
+document.getElementById('loginForm').onsubmit = function(event) { 
+    event.preventDefault();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let formData = {
+        email: email,
+        password: password
+    }
+    console.log(formData);
+};
+
+//SignUp Form Submit
+document.getElementById('signUpForm').onsubmit = function(event) { 
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let formData = {
+        name: name,
+        email: email,
+        password: password
+    }
+    console.log(formData);
+};
+
+
